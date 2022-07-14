3af6206a88437123f0edb254680b7b06afcc707f
Thomas Kinsella <thomasjkinsella4@gmail.com>
2022-07-14 19:13:01 -0400

    commit tree 6fdbfe2993d61d87ba8da740b3caf9b417784c77

--- index.js
+++ index.js
@@ -1,11 +1,72 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+// function scuberGreetingForFeet(feet){
+//   // Write your code here!
+//   if (feet < 400) {
+//     return "This one is on me!";
+//   } else if (feet > 2000 && feet < 2500) {
+//     return "I will gladly take your thirty bucks.";
+//   } else if (feet > 2500) {
+//     return "No can do.";
+//   };
+// };
+
+// function ternaryCheckCity(city){
+//   // Write your code here!
+//   return (city === "NYC") ? "Ok, sounds good." : "No go."
+// }
+
+// function switchOnCharmFromTip(tip){
+//   // Write your code here!
+//   switch (tip) {
+//     case "generous" :
+//       return "Thank you so much.";
+//     case "not as generous" :
+//       return "Thank you."
+//     default :
+//     return "Bye."
+//   }
+// }
+
+
+
+
+
+
+
+function scuberGreetingForFeet(distance) {
+    if (distance <= 400) {
+      return "This one is on me!";
+    } else if (distance > 2000 && distance <= 2500) {
+      return "I will gladly take your thirty bucks.";
+    } else if (distance > 2500) {
+      return "No can do.";
+    }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city) {
+  return city === "NYC" ? "Ok, sounds good." : "No go."
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip) {
+  if (tip === "generous") {
+    return "Thank you so much."
+  } else if (tip === "not as generous") {
+    return "Thank you."
+  } else {
+    return "Bye."
+  }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
